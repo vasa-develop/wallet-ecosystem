2c292c76b93454040f62d516f03b9c64130e0f10
vasa <vasa.develop@gmail.com>
2024-03-30 22:41:23 +0400

    chore: add nav and redirect for modules list

--- next.config.mjs
+++ next.config.mjs
@@ -12,17 +12,22 @@ const nextConfig = {
       },
     ],
   },
-  // async redirects() {
-  //   return [
-  //     // Basic redirect
-  //     {
-  //       source: '/standards',
-  //       destination:
-  //         'https://wallet-ecosystem.notion.site/Wallet-Ecosystem-Standards-Tracker-9151e788ab7243f7a53a70c6e9834b0c',
-  //       permanent: false,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/standards',
+        destination:
+          'https://wallet-ecosystem.notion.site/Wallet-Ecosystem-Standards-Tracker-9151e788ab7243f7a53a70c6e9834b0c',
+        permanent: false,
+      },
+      {
+        source: '/modules',
+        destination: 'https://wallet-ecosystem.notion.site/Smart-Account-Modules-8873025da42c49648bfe03bf980c89d6',
+        permanent: false,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,9 @@ export default function RootLayout({
                 <TabNav.Link href="https://wallet-ecosystem.notion.site/Wallet-Ecosystem-Standards-Tracker-9151e788ab7243f7a53a70c6e9834b0c">
                   Standards Tracker
                 </TabNav.Link>
+                <TabNav.Link href="https://wallet-ecosystem.notion.site/Smart-Account-Modules-8873025da42c49648bfe03bf980c89d6">
+                  Smart Account Modules List
+                </TabNav.Link>
               </TabNav.Root>
               <div />
             </div>
